c9a97ddd960b339e7263fb80f6a91bc623f834a1
yeni-dev <ethan.adg@proton.me>
2025-08-18 21:11:25 +0100

    updated comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import boto3
 
-aws_management_console = boto3.session.Session(profile_name="default")
+aws_management_console = boto3.session.Session(profile_name="default") #loga into management consle
 
-iam_console = aws_management_console.resource("iam")
+iam_console = aws_management_console.resource("iam") #moves to the iam dashboard
 
-for each_user in iam_console.users.all():
+for each_user in iam_console.users.all(): #lopps through and prints all users
     print(each_user.name)
