e19107ecd8c732e4551e1d3a4bd730327692feea
yeni-dev <ethan.adg@proton.me>
2025-08-18 21:49:54 +0100

    comparing client and resource objects

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import boto3
 
 aws_management_console = boto3.session.Session(profile_name="default") #loga into management consle
 
-iam_console = aws_management_console.resource("iam") #moves to the iam dashboard
+iam_console_resource = aws_management_console.resource("iam") #moves to the iam dashboard
 
-for each_user in iam_console.users.all(): #lopps through and prints all users
+iam_console_client = aws_management_console.client("iam") #client object
+
+
+
+
+
+for each_user in iam_console_resource.users.all(): #loops through and prints all users
     print(each_user.name)
+
+#iam user list with client object
+
+for each in iam_console_client.list_users()["Users"]:
+    print(each["UserName"])
